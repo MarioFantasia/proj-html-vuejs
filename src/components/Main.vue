c05f60becbc0a9a5ce67c53a80dc64ba8b3174eb
MarioFantasia <mariofantasia18@outlook.it>
2021-10-12 21:34:54 +0200

    section bottom

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,10 @@
             <SectionThree></SectionThree>
         </div>
             <SectionFour></SectionFour>
-        
+        <div class="container">
+            <!-- altre sezioni -->
+            <SectionBottom></SectionBottom>
+        </div>
         
     </main>
 </template>
@@ -16,13 +19,17 @@ import SectionTop from './MainComponents/SectionTop.vue'
 import SectionTutoring from './MainComponents/SectionTutoring.vue'
 import SectionThree from './MainComponents/SectionThree.vue'
 import SectionFour from './MainComponents/SectionFour.vue'
+/* altre sezioni */
+import SectionBottom from './MainComponents/SectionBottom.vue'
 export default {
     name : 'Main',
     components : {
         SectionTop,
         SectionTutoring,
         SectionThree,
-        SectionFour
+        SectionFour,
+        /* altre sezioni */
+        SectionBottom
     }
 }
 </script>
---
+++ src/components/MainComponents/SectionBottom.vue
@@ -0,0 +1,63 @@
+<template>
+    <section class="flex">
+            <div class="row row-cols-5">
+                <div class="image" v-for="(elm, index) in images" :key="index">
+                    <img class="out_hover" :src="require('../../assets/img/main/'+ elm.defaulf)" alt="img">
+                    <img class="on_hover" :src="require('../../assets/img/main/'+ elm.onHover)" alt="img">
+                </div>
+            </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name : 'SectionBottom',
+    data() {
+        return {
+            images : [
+                {
+                    defaulf : 'h5-client-1.png',
+                    onHover : 'h5-client-1-h.png'
+                },
+                {
+                    defaulf : 'h5-client-2.png',
+                    onHover : 'h5-client-2-h.png'
+                },
+                {
+                    defaulf : 'h5-client-3.png',
+                    onHover : 'h5-client-3-h.png'
+                },
+                {
+                    defaulf : 'h5-client-4.png',
+                    onHover : 'h5-client-4-h.png'
+                },
+                {
+                    defaulf : 'h5-client-5.png',
+                    onHover : 'h5-client-5-h.png'
+                },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    section {
+        height: 200px;
+        .row {
+            margin: auto 0;
+        }
+        .on_hover {
+                display: none;
+            }
+        .image:hover .out_hover{
+            display: none;
+        }
+        .image:hover .on_hover {
+            display: block;
+        }
+
+    }
+</style>
